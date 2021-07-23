19d2f4440afb3d8044cad4b7cd3112ae9ea51674
Aleksandar <aleksandar.lazic.p@gmail.com>
2021-07-23 22:32:43 +0200

    Added Basic styles with sass in Css/index.css file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import React from "react";
+import Navigation from "./components/Navigation/Navigation";
 
-function App() {
-	return <div className='App'></div>;
-}
+const App = () => {
+	return (
+		<React.Fragment>
+			<Navigation />
+		</React.Fragment>
+	);
+};
 
 export default App;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+	return (
+		<nav className='navigation-bar'>
+			<h2>Naslov</h2>
+		</nav>
+	);
+};
+
+export default Navigation;
